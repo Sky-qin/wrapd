5553d1856768ee324b97391970d3dab51505e3a2
Sky-qin <1095174004@qq.com>
2020-12-04 16:21:28 +0800

    save

---
+++ src/components/ViewLyout/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import T from "prop-types";
+
+const ViewLyout = {
+  count: 3,
+  ViewBox: (props) => {
+    const { children, count = 3 } = props;
+    ViewLyout.count = count;
+    return <div>{children}</div>;
+  },
+  ViewLabelItem: (props) => {
+    let { title, children } = props;
+    let { count } = ViewLyout;
+    let width = (1 / count) * 100;
+    return (
+      <div
+        style={{
+          display: "inline-block",
+          width: `${width}%`,
+          paddingRight: "20px",
+          paddingLeft: "12px",
+          lineHeight: "36px",
+        }}
+      >
+        <div style={{ display: "flex" }}>
+          <div style={{ paddingRight: "8px" }}>{title}</div>
+          <div style={{ paddingTop: "8px", flex: 1, lineHeight: "20px" }}>
+            {children}
+          </div>
+        </div>
+      </div>
+    );
+  },
+};
+
+ViewLyout.propTypes = {
+  count: T.number,
+};
+
+ViewLyout.defaultProps = {
+  count: 3,
+};
+
+ViewLyout.ViewBox.propTypes = {
+  count: T.number,
+};
+
+ViewLyout.ViewBox.defaultProps = {
+  count: 3,
+};
+
+ViewLyout.ViewLabelItem.propTypes = {
+  title: T.string,
+};
+
+ViewLyout.ViewLabelItem.defaultProps = {
+  title: "",
+};
+
+export default ViewLyout;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import TooltipButton from "./components/TooltipButton";
 import OpreationBar from "./components/OpreationBar";
 import ContentBox from "./components/ContentBox";
 import Ebuttons from "./components/Ebuttons";
+import ViewLyout from "./components/ViewLyout";
 
-export { Toolbar, TooltipButton, OpreationBar, ContentBox, Ebuttons };
+export {
+  Toolbar,
+  TooltipButton,
+  OpreationBar,
+  ContentBox,
+  Ebuttons,
+  ViewLyout,
+};
 export default {
   Toolbar,
   TooltipButton,
   OpreationBar,
   ContentBox,
   Ebuttons,
+  ViewLyout,
 };
